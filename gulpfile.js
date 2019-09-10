4231771ec2f561d6632cd534c2b78cd2d7bc10d2
Amy Kapernick <amy@aimhigherweb.design>
2019-09-10 08:40:17 +0800

    updated sass path

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 // gulpfile.js
-const gulp = require("gulp");
-const sass = require("gulp-sass");
+const gulp = require('gulp')
+const sass = require('gulp-sass')
 
 // a task to generate the css with sass
-gulp.task("css", function() {
-  return gulp
-    .src("./scss/main.scss")
-    .pipe(
-      sass({
-        outputStyle: "compressed"
-      }).on("error", sass.logError)
-    )
-    .pipe(gulp.dest("./css"));
-});
+gulp.task('css', function() {
+	return gulp
+		.src('./_includes/scss/main.scss')
+		.pipe(
+			sass({
+				outputStyle: 'compressed',
+			}).on('error', sass.logError)
+		)
+		.pipe(gulp.dest('./css'))
+})
 
-gulp.task("watch", function() {
-  gulp.watch("./scss/**/*.scss", gulp.parallel("css"));
-});
+gulp.task('watch', function() {
+	gulp.watch('./_includes/scss/**/*.scss', gulp.parallel('css'))
+})
