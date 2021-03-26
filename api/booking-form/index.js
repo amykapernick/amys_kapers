83811bb87b8ec250ef4db529cbda6d954fcab0d4
Amy Kapernick <amy@kapers.dev>
2021-03-26 09:10:34 +0800

    have form submission working

--- api/booking-form/index.js
+++ api/booking-form/index.js
@@ -1,5 +1,18 @@
+require('dotenv').config()
+
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    const types = {
+        days_2: '29 and 30 April - Full Workshop',
+        days_1_html_css: '29 April - Semantic HTML and CSS Layouts',
+        days_1_testing: '30 April - Front End Testing',
+        other: `I can't make this workshop, but want to know when the next one is`
+    }
 
     let params = {} 
     
@@ -9,14 +22,37 @@ module.exports = async function (context, req) {
         params[values[0].replace('+', ' ')] = values[1].replace('+', ' ');
     });
 
-    context.log(params)
+    const html = `
+    <p>New Workshop Booking:</p>
+    <ul>
+        <li>Name: ${params.name}</li>
+        <li>Email: ${params.email}</li>
+        <li>Company: ${params.company}</li>
+        <li>Tickets: ${params.tickets}</li>
+        <li>Ticket Type: ${types[params.type]}</li>
+        <li>Notes: <p>${params.notes.replace('+', ' ')}</p></li>
+    </ul>
+`
+
+    const msg = {
+        to: process.env.FORM_EMAIL,
+        from: {
+            name: params.name,
+            email: params.email
+        },
+        subject: `New Workshop Booking: ${params.name}`,
+        html
+    }
+
+    context.log(msg)
+
+    await sgMail.send(msg)
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         status: 302,
         headers: {
             location: process.env.BOOKING_REDIRECT
-        },
-        body: params
+        }
     };
 }
