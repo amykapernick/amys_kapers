47e453cc1550bb98987bb35ab4d0a7cb607328a6
Amy Kapernick <amy@kapers.dev>
2021-03-26 09:15:42 +0800

    updated env variables

--- site/_data/env.js
+++ site/_data/env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	env: process.env.ELEVENTY_ENV,
 	api: process.env.API_URL,
-	siteUrl: process.env.SITE_URL || 'https://www.amyskapers.dev',
+	siteUrl: process.env.SITE_URL,
 	formWorkshopBooking: process.env.FORM_WORKSHOP_BOOKING
 }
