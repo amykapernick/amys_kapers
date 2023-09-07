82ac33c11abab461999fab289f89f7b9c138cda3
Amy Kapernick <amy@kapers.dev>
2023-09-07 10:32:56 +0800

    pull events data from notion

--- api/events/index.js
+++ api/events/index.js
@@ -1,28 +1,55 @@
-const { parse, compareAsc, isBefore, isSameDay } = require('date-fns')
+require('dotenv').config()
+const { parse, compareAsc, set } = require('date-fns')
 const formatDate = require('../utils/formatDate')
-const slugify = (string) => string.toLowerCase().replace(' ', '-')
+const { Client } = require('@notionhq/client')
+const notion = new Client({ auth: process.env.NOTION_EVENTS_API })
 
 module.exports = async function (context, req) {
-    const events = require('../_data/events.json')
     const today = new Date()
     const filter = req?.query?.filter
-
-    let currentEvents = events
-
-    if (filter !== 'all') {
-        currentEvents = currentEvents
-            .filter((event) => {
-                const date = event.end ? parse(event.end, 'yyyy-MM-dd', new Date()) : parse(event.start, 'yyyy-MM-dd', new Date())
-
-                return isSameDay(today, date) || isBefore(today, date)
-            })
+    let eventsFilter = {
+        and: [
+            {
+                property: 'Conference',
+                date: {
+                    on_or_after: today
+                }
+            }
+        ]
     }
 
-    currentEvents = currentEvents
-        .sort((a, b) => (compareAsc(
-            parse(a.start, 'yyyy-MM-dd', new Date()),
-            parse(b.start, 'yyyy-MM-dd', new Date())
-        )))
+    if (filter === 'all') {
+        eventsFilter = {
+            and: [
+                {
+                    property: 'Conference',
+                    date: {
+                        on_or_after: set(today, { month: 0, date: 1 })
+                    }
+                }
+            ]
+        }
+    }
+
+    const notionEvents = await notion.databases.query({
+        database_id: process.env.EVENTS_DB_ID,
+        filter: eventsFilter,
+        sorts: [
+            {
+                property: 'Conference',
+                direction: 'ascending'
+            }
+        ]
+    })
+
+    const events = notionEvents.results.map(({ properties }) => ({
+        start: properties.Conference.date.start,
+        end: properties.Conference.date.end,
+        name: properties.Name.title[0].plain_text,
+        type: properties.Type.multi_select.map(({ name }) => name.toLowerCase()),
+        url: properties.URL.url,
+        hidden: properties.Hidden.checkbox,
+    }))
         .map(event => ({
             ...event,
             start: formatDate(event.start, 'dd MMM'),
@@ -31,16 +58,10 @@ module.exports = async function (context, req) {
                 end: event.end && formatDate(event.end, 'yyyy-MM-dd'),
                 start: formatDate(event.start, 'yyyy-MM-dd')
             },
+            hidden: event.type?.length < 1 ? true : event.hidden
         }))
 
-    const eventData = currentEvents.map(({ type = ['attending'], ...event_args }) => ({
-        ...event_args,
-        type: Array.isArray(type) ? type : [type],
-
-    }))
-
-
     context.res = {
-        body: eventData
+        body: events
     };
 }
