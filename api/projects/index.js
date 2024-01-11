15023c890cfda6136942c7053c8df36d23aadf8d
Amy Kapernick <amy.kapernick@makerx.com.au>
2024-01-11 17:59:41 +0800

    fix errors

---
+++ api/projects/index.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+const { set } = require('date-fns')
+const formatDate = require('../utils/formatDate')
+const { Client } = require('@notionhq/client')
+const notion = new Client({ auth: process.env.NOTION_WORK_API })
+
+module.exports = async function (context, req) {
+    const today = new Date()
+    const filter = req?.query?.filter
+
+    let eventsFilter = {
+        and: [
+            {
+                property: 'Conference',
+                date: {
+                    on_or_after: today
+                }
+            }
+        ]
+    }
+
+    if (filter === 'all') {
+        eventsFilter = {
+            and: [
+                {
+                    property: 'Conference',
+                    date: {
+                        on_or_after: set(today, { month: 0, date: 1 })
+                    }
+                }
+            ]
+        }
+    }
+
+    const notionProjects = await notion.databases.query({
+        database_id: process.env.PROJECTS_DB_ID,
+        // filter: eventsFilter,
+        // sorts: [
+        //     {
+        //         property: 'Conference',
+        //         direction: 'ascending'
+        //     }
+        // ]
+    })
+
+    console.log({ notionProjects })
+
+    const events = notionProjects.results.map(({ properties }) => ({
+        start: properties.Conference.date.start,
+        end: properties.Conference.date.end,
+        name: properties.Name.title[0].plain_text,
+        type: properties.Type.multi_select.map(({ name }) => name.toLowerCase()),
+        url: properties.URL.url,
+        hidden: properties.Hidden.checkbox,
+    }))
+        .map(event => ({
+            ...event,
+            start: formatDate(event.start, 'dd MMM'),
+            end: event.end ? ` - ${formatDate(event.end, 'dd MMM')}` : '',
+            dates: {
+                end: event.end && formatDate(event.end, 'yyyy-MM-dd'),
+                start: formatDate(event.start, 'yyyy-MM-dd')
+            },
+            hidden: event.type?.length < 1 ? true : event.hidden
+        }))
+
+    context.res = {
+        body: events
+    };
+}
