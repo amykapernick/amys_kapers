a34d54d9d1974256662923253cd6d8ded40559b6
Amy Kapernick <amy@kapers.dev>
2024-02-12 11:14:59 +0800

    added stats for embedding route

---
+++ api/stats/index.js
@@ -0,0 +1,57 @@
+const { Client } = require('@notionhq/client')
+const notion = new Client({ auth: process.env.NOTION_EVENTS_API })
+
+module.exports = async function (context, req) {
+    const notionInvites = await notion.databases.query({
+        database_id: process.env.INVITES_DB_ID,
+        filter: {
+            property: 'Status',
+            status: {
+                does_not_equal: 'Not Invited'
+            }
+
+        }
+    })
+    const statusCounts = {}
+    const attendeeCounts = {
+        'Attending': {
+            Adults: 0,
+            Children: 0
+        },
+        'Not Attending': {
+            Adults: 0,
+            Children: 0
+        },
+        "Pending": {
+            Adults: 0,
+            Children: 0
+        }
+    }
+
+    const invites = notionInvites?.results?.map(({ properties }) => ({
+        ...properties
+    }))
+
+    invites.forEach(invite => {
+        const status = invite?.Status?.status?.name
+
+        if (!statusCounts[status]) statusCounts[status] = 0
+
+        attendeeCounts['Attending'].Adults += invite?.['Adults RSVPed']?.formula?.number || 0
+        attendeeCounts['Attending'].Children += invite?.['Children RSVPed']?.formula?.number || 0
+        attendeeCounts['Not Attending'].Adults += invite?.['Adults Declined']?.formula?.number || 0
+        attendeeCounts['Not Attending'].Children += invite?.['Children Declined']?.formula?.number || 0
+        attendeeCounts['Pending'].Adults += invite?.['Adults Pending']?.formula?.number || 0
+        attendeeCounts['Pending'].Children += invite?.['Children Pending']?.formula?.number || 0
+
+        statusCounts[status]++
+    })
+
+    context.res = {
+        status: 200,
+        body: {
+            statusCounts,
+            attendeeCounts,
+        }
+    };
+}
---
+++ src/react/stats.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+const colours = {
+	'Not Attending': '#AD1728',
+	'Attending': '#275942',
+	'Pending': '#364c93',
+	'Invited': '#364c93',
+	'Invitation': '#BB5C04',
+	'Declined': '#AD1728',
+	'RSVPed': '#275942',
+}
+
+const TravelFeed = (props) => {
+	const { api } = props
+	const [data, setData] = useState(false)
+
+	useEffect(() => {
+		fetch(`${api}/stats`, {
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		})
+			.then((res) => res.json())
+			.then(res => setData(res))
+	}, [])
+
+	return (
+		<>
+			{!data 
+				? <p>Loading stats...</p>
+				: <dl>
+					{Object.entries(data?.statusCounts).map(([status, count]) => (
+						<span style={{'--colour': colours[status]}}>
+							<dt>{status}</dt>
+							<dd>{count}</dd>
+						</span>
+					))}
+					{Object.entries(data?.attendeeCounts).map(([status, counts]) => (
+						<span className="double" style={{'--colour': colours[status]}}>
+							<dt>{status}</dt>
+							<dd>
+								<dl>
+									{Object.entries(counts).map(([attendee, count]) => (
+										<span>
+											<dt>{attendee}</dt>
+											<dd>{count}</dd>
+										</span>
+									))}	
+								</dl>	
+							</dd>
+						</span>
+					))}
+				</dl>
+			}
+			
+		</>
+	)
+}
+
+export default TravelFeed
